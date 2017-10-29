5d2066f432dc54cb68b3f3544f92eeace84e983a
Jakub Benes <jukben@gmail.com>
2017-10-29 23:04:25 +0100

    test(cypress): added cypress :heart:

--- example/App.jsx
+++ example/App.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import ReactTextareaAutocomplete from '@webscopeio/react-textarea-autocomplete';
 import emoji from '@jukben/emoji-search';
 
-import '@webscopeio/react-textarea-autocomplete/style.css'
+// import '@webscopeio/react-textarea-autocomplete/style.css'
+import '../style.css';
 
 const Item = ({ entity: { name, char } }) => <div>{`${name}: ${char}`}</div>;
 const Loading = ({ data }) => <div>Loading</div>;
@@ -27,9 +28,10 @@ class App extends Component {
           minChar={0}
           trigger={{
             ':': {
-              dataProvider: token => emoji(token)
-                .slice(0, 10)
-                .map(({ name, char }) => ({ name, char })),
+              dataProvider: token =>
+                emoji(token)
+                  .slice(0, 10)
+                  .map(({ name, char }) => ({ name, char })),
               component: Item,
               output: (item, trigger) => item.char,
             },
--- webpack.dev.js
+++ webpack.dev.js
@@ -3,6 +3,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './example/index.js',
+  devServer: {
+    port: 8080,
+  },
   resolve: {
     alias: {
       '@webscopeio/react-textarea-autocomplete': path.resolve(__dirname, './src'),
@@ -12,13 +15,11 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.jsx?$/, use: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.css$/,
-        use: [
-          { loader: 'style-loader' },
-          { loader: 'css-loader' },
-        ],
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
       },
     ],
   },
-  plugins: [ new HtmlWebpackPlugin({ template: './example/index.html'}) ],
+  plugins: [new HtmlWebpackPlugin({ template: './example/index.html' })],
 };
